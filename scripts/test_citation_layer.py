2475f45de20638f8b747439f44bf3c11c358d25d
Ge Zhang <ZG381492242@163.com>
2026-09-26 19:55:23 +0800

    feat: add COVID-HF citation layer

--- scripts/test_citation_layer.py
+++ scripts/test_citation_layer.py
@@ -18,6 +18,7 @@ TARGETS = {
     "doi-10-1172-jci194175": ("doi-10-1172-jci194175.json", 5, 7, 7),
     "olink-dcm": ("olink-dcm.json", 5, 6, 6),
     "smc-fate": ("smc-fate.json", 6, 6, 6),
+    "doi-10-1002-ehf2-14003": ("doi-10-1002-ehf2-14003.json", 5, 6, 6),
 }
 
 
